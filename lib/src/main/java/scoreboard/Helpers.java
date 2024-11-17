166ceed4367b7b8ca24a49e8b0f6fbf204fbabe3
Balázs Söptei <bsoptei@gmail.com>
2024-11-17 23:15:59 +0100

    implement Helpers

--- lib/src/main/java/scoreboard/Helpers.java
+++ lib/src/main/java/scoreboard/Helpers.java
@@ -5,10 +5,24 @@ import java.util.List;
 public class Helpers {
 
     public static String matchPrettyString(Match match) {
-        return null;
+        return String.format(
+                "%s %s - %s %s",
+                match.homeTeam(),
+                match.homeScore(),
+                match.awayTeam(),
+                match.awayScore()
+        );
     }
 
     public static String summaryPrettyString(List<Match> matches) {
-        return null;
+        var sb = new StringBuilder();
+
+        var it = matches.listIterator();
+
+        while (it.hasNext()) {
+            sb.append(String.format("%s. %s\n", it.nextIndex() + 1, matchPrettyString(it.next())));
+        }
+
+        return sb.toString();
     }
 }
